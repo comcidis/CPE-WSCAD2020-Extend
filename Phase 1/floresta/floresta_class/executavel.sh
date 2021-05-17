625a5bc24823a5392f7596fbe10bcb7f7758525b
Vitor Gonçalves Vieira <vitorgoncalvesvieira22@gmail.com>
2021-05-17 13:00:41 -0300

    arvore reg atualizada

--- Phase 1/floresta/floresta_class/executavel.sh
+++ Phase 1/floresta/floresta_class/executavel.sh
@@ -12,7 +12,7 @@ for k in $(seq 3)
 do
 	EXECUCAOPERF="sudo time perf stat -x | -e /power/energy-pkg/,/power/energy-cores/,/power/energy-gpu/"
 	COMANDO2="sudo perf stat -x | -e instructions,cycles,cpu-clock,cpu-migrations,branches,branch-misses,context-switches,bus-cycles,cache-references,cache-misses,mem-loads,mem-stores,L1-dcache-stores,L1-dcache-loads,L1-dcache-load-misses,L1-dcache-store-misses,LLC-stores,LLC-store-misses,LLC-loads,LLC-load-misses,minor-faults,page-faults"
-	APP="python3.7 floresta_clas.py ${EXEMPLO[j]} ${ATRIBUTO[k]}"
+	APP="python3.7 floresta_class.py ${EXEMPLO[j]} ${ATRIBUTO[k]}"
         $EXECUCAOPERF $COMANDO2 $APP &>Resultado_${EXEMPLO[j]}${ATRIBUTO[k]}ATT$[i].txt
 if [ -d "${EXEMPLO[j]}${ATRIBUTO[k]}" ]
 then
